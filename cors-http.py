8a1a14c26b0f6952b6d7ec788b9226b894f73e47
Luuk van Berkel <lah.vanberkel@student.avans.nl>
2024-10-18 11:04:35 +0200

    Working csp

--- cors-http.py
+++ cors-http.py
@@ -2,10 +2,12 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 import sys
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
-        self.send_header('access-control-allow-origin', 'self')
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        csp_policy = "default-src 'self'; font-src 'self'; style-src 'self'; script-src 'self';"
+        self.send_header('Content-Security-Policy', csp_policy)
         SimpleHTTPRequestHandler.end_headers(self)
+        
 
 if __name__ == '__main__':
     test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
